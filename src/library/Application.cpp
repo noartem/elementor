88061a0277ac6dc61e104e60f852dcb425604e7f
Noskov Artem <nowasmawesome@gmail.com>
2022-04-14 23:05:08 +0500

    Add Application draw example

--- src/library/Application.cpp
+++ src/library/Application.cpp
@@ -6,5 +6,14 @@
 
 namespace elementor {
     void Application::draw(SkCanvas *canvas) {
+        SkPaint paint;
+        paint.setColor(SK_ColorWHITE);
+        canvas->drawPaint(paint);
+
+        paint.setColor(SK_ColorBLUE);
+        canvas->drawRect({100, 200, 300, 500}, paint);
+
+        paint.setColor(SK_ColorRED);
+        canvas->drawRect({200, 200, 600, 500}, paint);
     }
 }
