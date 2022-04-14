d469d5399e802456b33023be61b31119baa77fea
Noskov Artem <nowasmawesome@gmail.com>
2022-04-14 23:05:01 +0500

    Edit main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
+//
+// Created by noartem on 14.04.2022.
+//
 
+#include "./library/Platform.h"
 #include "./library/Padding.h"
-#include "./library/IApplication.h"
 
-int main(int argc, char **argv) {
+int main(void) {
+	elementor::Platform platform;
+	platform.title = "Elementor-test";
+	platform.width = 960;
+	platform.height = 640;
+
     elementor::Padding root_el;
 
-    return 0;
+	platform.run();
 }
