dc2c4cfac53657264005813e73baa28d761e8694
Noskov Artem <nowasmawesome@gmail.com>
2022-04-19 22:05:46 +0500

    Refactor Flex

--- src/library/Flex.cpp
+++ src/library/Flex.cpp
@@ -3,11 +3,15 @@
 //
 
 #include "Flex.h"
+#include "Flexible.h"
+
+#include <tuple> 
+#include <algorithm>
 
 namespace elementor {
-    std::unique_ptr <ElementRenderer> Flex::render() {
-        auto renderer = std::make_unique<FlexRenderer>();
-        renderer->gap = this->gap;
+    std::shared_ptr <ElementRenderer> Flex::render() {
+        auto renderer = std::make_shared<FlexRenderer>();
+        renderer->spacing = this->spacing;
         renderer->direction = this->direction;
         renderer->children = this->children;
         return renderer;
@@ -16,28 +20,62 @@ namespace elementor {
     std::vector <RenderElement> FlexRenderer::getChildren(RenderSize size) {
         std::vector <RenderElement> children;
 
-        int axisSize = this->direction == FlexDirection::Row ? size.width : size.height;
+        RenderBoundaries sizedChildBoundaries = {{0, 0}, size};
+
         int childrenCount = this->children.size();
-        int itemSize = (axisSize - (childrenCount - 1) * this->gap) / childrenCount;
+        int fixedSize = this->spacing * (childrenCount - 1);
+
+        std::vector <std::tuple <int, int>> flexibleChildren;
+        int flexibleGrowsSum = 0;
 
         for (int i = 0; i < childrenCount; i++) {
             RenderElement child;
             child.element = this->children[i];
+            child.renderer = child.element->render();
 
-            int axisPosition = (itemSize + this->gap) * i;
+            Flexible *childAsFlexible = dynamic_cast<Flexible*>(child.element);
+            if (childAsFlexible) {
+                std::tuple<int, int> flexibleChild (i, childAsFlexible->grow);
+                flexibleChildren.push_back(flexibleChild);
+                flexibleGrowsSum += childAsFlexible->grow;
+            } else {
+                child.size = child.renderer->getSize(sizedChildBoundaries);
+                fixedSize += this->direction == FlexDirection::Row ? child.size.width : child.size.height;
+            }
+
+            children.push_back(child);
+        }
+
+        int axisSize = this->direction == FlexDirection::Row ? size.width : size.height;
+        int freeSize = axisSize - fixedSize;
+        int sizePerGrow = freeSize / std::max(flexibleGrowsSum, 1);
+
+        for (std::tuple <int, int> flexibleChild : flexibleChildren) {
+            int childIndex = std::get<0>(flexibleChild);
+            RenderElement &child = children[childIndex];
+
+            int childGrow = std::get<1>(flexibleChild);
+            int axisSize = sizePerGrow * childGrow;
+            RenderBoundaries childBoundaries;
+            if (this->direction == FlexDirection::Row) {
+                childBoundaries = {{axisSize, 0}, {axisSize, size.height}};
+            } else {
+                childBoundaries = {{0, axisSize}, {size.width, axisSize}};
+            }
+
+            child.size = child.renderer->getSize(childBoundaries);
+        }
+
+        int axisPosition = 0;
+        for (RenderElement &child : children) {
             if (this->direction == FlexDirection::Row) {
                 child.position = {axisPosition, 0};
             } else {
                 child.position = {0, axisPosition};
             }
 
-            if (this->direction == FlexDirection::Row) {
-                child.size = {itemSize, size.height};
-            } else {
-                child.size = {size.width, itemSize};
-            }
-
-            children.push_back(child);
+            axisPosition += this->spacing;
+            axisPosition += this->direction == FlexDirection::Row ? child.size.width : child.size.height;
         }
 
         return children;
--- src/library/Flex.h
+++ src/library/Flex.h
@@ -15,16 +15,16 @@ namespace elementor {
 
     class Flex : public Element {
     public:
-        int gap = 0;
+        int spacing = 0;
         FlexDirection direction = FlexDirection::Row;
         std::vector<Element *> children;
 
-        std::unique_ptr <ElementRenderer> render() override;
+        std::shared_ptr <ElementRenderer> render() override;
     };
 
     class FlexRenderer : public ElementRenderer {
     public:
-        int gap = 0;
+        int spacing = 0;
         FlexDirection direction = FlexDirection::Row;
         std::vector<Element *> children;
 
