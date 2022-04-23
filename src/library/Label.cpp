0d45395d3db86d24a699b59699cc9787a4df62c9
Noskov Artem <nowasmawesome@gmail.com>
2022-04-23 15:16:01 +0500

    Refactor Label

--- src/library/Label.cpp
+++ src/library/Label.cpp
@@ -4,41 +4,109 @@
 
 #include "Label.h"
 
+#include <algorithm>
+
 namespace elementor {
+    Label::Label(std::string text) {
+        this->setText(text);
+    }
+
+    Label::Label(std::string text, SkColor fontColor) {
+        this->setText(text);
+        this->setFontColor(fontColor);
+    }
+
+    Label::Label(std::string text, std::string fontColor) {
+        this->setText(text);
+        this->setFontColor(fontColor);
+    }
+
+    Label::Label(std::string text, float fontSize) {
+        this->setText(text);
+        this->setFontSize(fontSize);
+    }
+
+    Label::Label(std::string text, SkColor fontColor, float fontSize) {
+        this->setText(text);
+        this->setFontColor(fontColor);
+        this->setFontSize(fontSize);
+    }
+
+    Label::Label(std::string text, std::string fontColor, float fontSize) {
+        this->setText(text);
+        this->setFontColor(fontColor);
+        this->setFontSize(fontSize);
+    }
+
+    void Label::setText(std::string text) {
+        this->text = text;
+    }
+
+    std::string Label::setText() {
+        return this->text;
+    }
+
+    void Label::setFontColor(SkColor color) {
+        this->fontColor = color;
+    }
+
+    void Label::setFontColor(uint8_t r, uint8_t g, uint8_t b) {
+        this->setFontColor(SkColorSetRGB(r, g, b));
+    }
+
+    void Label::setFontColor(std::string hex) {
+        if (hex.size() == 7) {
+            hex = hex.substr(1);
+        }
+
+        if (hex.size() != 6) {
+            return;
+        }
+
+        uint8_t r = std::stoul(hex.substr(0, 2), nullptr, 16);
+        uint8_t g = std::stoul(hex.substr(2, 2), nullptr, 16);
+        uint8_t b = std::stoul(hex.substr(4, 2), nullptr, 16);
+        this->setFontColor(r, g, b);
+    }
+
+    SkColor Label::getFontColor() {
+        return this->fontColor;
+    }
+
+    void Label::setFontSize(float size) {
+        this->fontSize = size;
+    }
+
+    float Label::getFontSize() {
+        return this->fontSize;
+    }
+
     std::shared_ptr <ElementRenderer> Label::render() {
-        auto renderer = std::make_shared<LabelRenderer>();
-        renderer->text = this->text;
-        renderer->fontSize = this->fontSize;
-        renderer->fontColor = this->fontColor;
-        renderer->context = this->context;
-        return renderer;
+        return std::make_shared<LabelRenderer>(this->context, this->text, this->fontColor, this->fontSize);
     }
 
-    SkFont LabelRenderer::makeFont() {
-        SkFont font;
-        font.setSize(this->fontSize * this->context->monitorPixelScale);
-        return font;
-    }
-
-    SkRect LabelRenderer::makeTextBounds() {
-        SkRect bounds;
-        this->makeFont().measureText(this->text.c_str(), this->text.size(), SkTextEncoding::kUTF8, &bounds);
-        return bounds;
+    LabelRenderer::LabelRenderer(ApplicationContext *context, std::string text, SkColor fontColor, float fontSize) {
+        this->context = context;
+        this->text = text;
+        this->fontColor = fontColor;
+        this->fontSize = fontSize * this->context->monitorPixelScale;
+        this->font.setSize(fontSize);
+        this->font.measureText(this->text.c_str(), this->text.size(), SkTextEncoding::kUTF8, &this->bounds);
     }
 
     RenderSize LabelRenderer::getSize(RenderBoundaries boundaries) {
-        SkRect bounds = this->makeTextBounds();
-        return {(int) ceil(bounds.width()), (int) ceil(bounds.height())};
+        int textWidth = ceil(this->bounds.width());
+        int textHeight = ceil(this->bounds.height());
+        int width = std::min(std::max(textWidth, boundaries.min.width), boundaries.max.width);
+        int height = std::min(std::max(textHeight, boundaries.min.height), boundaries.max.height);
+        return {width, height};
     }
 
     void LabelRenderer::paintBackground(SkCanvas *canvas, RenderSize size) {
-        SkFont font = this->makeFont();
-
         SkPaint paint;
         paint.setColor(this->fontColor);
 
-        SkRect bounds = this->makeTextBounds();
-        canvas->translate(0, bounds.height());
-        canvas->drawString(this->text.c_str(), 0, 0, font, paint);
+        canvas->translate(0, this->bounds.height());
+        canvas->drawString(this->text.c_str(), 0, 0, this->font, paint);
     }
 }
--- src/library/Label.h
+++ src/library/Label.h
@@ -12,26 +12,58 @@
 namespace elementor {
     class Label : public Element {
     public:
+        Label() {};
+
+        Label(std::string text);
+
+        Label(std::string text, SkColor fontColor);
+
+        Label(std::string text, std::string fontColor);
+
+        Label(std::string text, float fontSize);
+
+        Label(std::string text, SkColor fontColor, float fontSize);
+
+        Label(std::string text, std::string fontColor, float fontSize);
+
+        void setText(std::string text);
+
+        std::string setText();
+
+        void setFontColor(SkColor color);
+
+        void setFontColor(uint8_t r, uint8_t g, uint8_t b);
+
+        void setFontColor(std::string color);
+
+        SkColor getFontColor();
+
+        void setFontSize(float size);
+
+        float getFontSize();
+
+        std::shared_ptr <ElementRenderer> render() override;
+
+    private:
         std::string text;
         SkColor fontColor = SK_ColorBLACK;
         float fontSize = 16.0;
-
-        std::shared_ptr <ElementRenderer> render() override;
     };
 
     class LabelRenderer : public ElementRenderer {
     public:
-        std::string text;
-        SkColor fontColor;
-        float fontSize;
+        LabelRenderer(ApplicationContext *context, std::string text, SkColor fontColor, float fontSize);
 
         RenderSize getSize(RenderBoundaries boundaries) override;    
 
         void paintBackground(SkCanvas *canvas, RenderSize size) override;
 
     private:
-        SkFont makeFont();
-        SkRect makeTextBounds();
+        std::string text;
+        SkColor fontColor;
+        float fontSize;
+        SkFont font;
+        SkRect bounds;
     };
 }
 
