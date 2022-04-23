9dc728d2ca5bf47edea374b9dd3d97ed2fbe892a
Noskov Artem <nowasmawesome@gmail.com>
2022-04-23 14:38:17 +0500

    Refactor Sized

--- src/library/Sized.cpp
+++ src/library/Sized.cpp
@@ -7,20 +7,59 @@
 #include <algorithm>
 
 namespace elementor {
+    Sized::Sized(Element *child) {
+        this->setChild(child);
+    }
+
+    Sized::Sized(float size, Element *child) {
+        this->setSize(size);
+        this->setChild(child);
+    }
+
+    Sized::Sized(float width, float height, Element *child) {
+        this->setSize(width, height);
+        this->setChild(child);
+    }
+
+    void Sized::setWidth(float width) {
+        this->width = width;
+    }
+
+    void Sized::setHeight(float height) {
+        this->height = height;
+    }
+
+    void Sized::setSize(float width, float height) {
+        this->setWidth(width);
+        this->setHeight(height);
+    }
+
+    void Sized::setSize(float size) {
+        this->setSize(size, size);
+    }
+
+    float Sized::getWidth() {
+        return this->width;
+    }
+
+    float Sized::getHeight() {
+        return this->height;
+    }
+
     std::shared_ptr <ElementRenderer> Sized::render() {
-        auto renderer = std::make_shared<SizedRenderer>();
-        renderer->width = this->width;
-        renderer->height = this->height;
-        renderer->child = this->child;
-        renderer->context = this->context;
-        return renderer;
+        return std::make_shared<SizedRenderer>(this->context, this->width, this->height, this->getChild());
+    }
+
+    SizedRenderer::SizedRenderer(ApplicationContext *context, float width, float height, Element *child) {
+        this->context = context;
+        this->width = width * this->context->monitorPixelScale;
+        this->height = height * this->context->monitorPixelScale;
+        this->child = child;
     }
 
     RenderSize SizedRenderer::getSize(RenderBoundaries boundaries) {
-        int sizeWidth = this->width * this->context->monitorPixelScale;
-        int width = std::min(std::max(sizeWidth, boundaries.min.width), boundaries.max.width);
-        int sizeHeight = this->height * this->context->monitorPixelScale;
-        int height = std::min(std::max(sizeHeight, boundaries.min.height), boundaries.max.height);
+        int width = std::min(std::max(this->width, boundaries.min.width), boundaries.max.width);
+        int height = std::min(std::max(this->height, boundaries.min.height), boundaries.max.height);
         return {width, height};
     }
 
--- src/library/Sized.h
+++ src/library/Sized.h
@@ -8,24 +8,47 @@
 #include "Element.h"
 
 namespace elementor {
-    class Sized : public Element {
+    class Sized : public Element, public WithChild {
     public:
-        float width = 0;
-        float height = 0;
-        Element *child;
+        Sized() {};
+
+        Sized(Element *child);
+
+        Sized(float size, Element *child);
+
+        Sized(float width, float height, Element *child);
+
+        void setWidth(float width);
+
+        void setHeight(float height);
+
+        void setSize(float width, float height);
+
+        void setSize(float size);
+
+        float getWidth();
+
+        float getHeight();
 
         std::shared_ptr <ElementRenderer> render() override;
+
+    private:
+        float width = 0;
+        float height = 0;
     };
 
     class SizedRenderer : public ElementRenderer {
     public:
-        float width = 0;
-        float height = 0;
-        Element *child;
+        SizedRenderer(ApplicationContext *context, float width, float height, Element *child);
 
         RenderSize getSize(RenderBoundaries boundaries) override;
 
         std::vector <RenderElement> getChildren(RenderSize size) override;
+
+    private:
+        int width = 0;
+        int height = 0;
+        Element *child;
     };
 }
 
