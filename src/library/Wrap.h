fdf72f8ac2e52d64451e137d279c1dbab54ab626
Noskov Artem <nowasmawesome@gmail.com>
2022-05-04 20:52:51 +0500

    Add Wrap

---
+++ src/library/Wrap.h
@@ -0,0 +1,46 @@
+//
+// Created by noartem on 04.05.2022.
+//
+
+#ifndef ELEMENTOR_WRAP_H
+#define ELEMENTOR_WRAP_H
+
+#include "Element.h"
+
+namespace elementor {
+    enum class WrapDirection {
+        Row,
+        Column,
+    };
+
+    class Wrap : public Element, WithChildren {
+    public:
+        Wrap *setSpacing(float spacing);
+
+        float getSpacing();
+
+        Wrap *setCrossSpacing(float spacing);
+
+        float getCrossSpacing();
+
+        Wrap *setSpacing(float spacing, float crossSpacing);
+
+        Wrap *setDirection(WrapDirection direction);
+
+        WrapDirection getDirection();
+
+        Wrap *appendChild(Element *child);
+
+        std::vector <RenderElement> getRenderChildren(RenderSize size) override;
+
+    private:
+        float spacing = 0;
+        float crossSpacing = 0;
+        WrapDirection direction = WrapDirection::Row;
+    };
+
+    Wrap *wrap();
+}
+
+
+#endif //ELEMENTOR_WRAP_H
