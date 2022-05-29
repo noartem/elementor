dc58f69333d598ca47073bd7e3fc8350ffea8b2d
Noskov Artem <nowasmawesome@gmail.com>
2022-05-29 14:44:58 +0500

    Add utility.h

---
+++ src/library/utility.h
@@ -0,0 +1,31 @@
+//
+// Created by noartem on 29.05.2022.
+//
+
+#ifndef ELEMENTOR_UTILITY_H
+#define ELEMENTOR_UTILITY_H
+
+#include "Align.h"
+#include "Application.h"
+#include "Background.h"
+#include "Border.h"
+#include "Clickable.h"
+#include "Column.h"
+#include "Element.h"
+#include "Event.h"
+#include "Flex.h"
+#include "Flexible.h"
+#include "Hoverable.h"
+#include "Label.h"
+#include "Padding.h"
+#include "Platform.h"
+#include "Render.h"
+#include "Rounded.h"
+#include "Row.h"
+#include "Scrollable.h"
+#include "Scrollbar.h"
+#include "Sized.h"
+#include "utility.h"
+#include "Wrap.h"
+
+#endif //ELEMENTOR_UTILITY_H
