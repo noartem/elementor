f68140f7ef7217424aead84c63fdafa976d8fff0
Noskov Artem <nowasmawesome@gmail.com>
2022-06-11 15:56:56 +0500

    Add Text

---
+++ src/library/elements/Text.cpp
@@ -0,0 +1,215 @@
+//
+// Created by noartem on 11.06.2022.
+//
+
+#include "Text.h"
+
+#include <include/core/SkTypeface.h>
+
+#include <algorithm>
+
+namespace elementor::elements {
+    Text *text() {
+        return new Text();
+    }
+
+    Text *Text::setText(std::string text) {
+        this->text = text;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    std::string Text::setText() {
+        return this->text;
+    }
+
+    Text *Text::setFontColor(SkColor color) {
+        this->fontColor = color;
+        this->paint = std::nullopt;
+        return this;
+    }
+
+    Text *Text::setFontColor(uint8_t r, uint8_t g, uint8_t b) {
+        this->setFontColor(SkColorSetRGB(r, g, b));
+        return this;
+    }
+
+    Text *Text::setFontColor(std::string hex) {
+        if (hex.size() == 7) {
+            hex = hex.substr(1);
+        }
+
+        if (hex.size() == 6) {
+            uint8_t r = std::stoul(hex.substr(0, 2), nullptr, 16);
+            uint8_t g = std::stoul(hex.substr(2, 2), nullptr, 16);
+            uint8_t b = std::stoul(hex.substr(4, 2), nullptr, 16);
+            this->setFontColor(r, g, b);
+        }
+
+        return this;
+    }
+
+    SkColor Text::getFontColor() {
+        return this->fontColor;
+    }
+
+    Text *Text::setFontSize(float size) {
+        this->fontSize = size;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    float Text::getFontSize() {
+        return this->fontSize;
+    }
+
+    Text *Text::setFontSkew(float skew) {
+        this->fontSkew = skew;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    float Text::getFontSkew() {
+        return this->fontSkew;
+    }
+
+    Text *Text::setFontScale(float scale) {
+        this->fontScale = scale;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    float Text::getFontScale() {
+        return this->fontScale;
+    }
+
+    Text *Text::setFontWeight(float weight) {
+        this->fontWeight = weight;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    float Text::getFontWeight() {
+        return this->fontWeight;
+    }
+
+    Text *Text::setFontWidth(float width) {
+        this->fontWidth = width;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    float Text::getFontWidth() {
+        return this->fontWidth;
+    }
+
+    Text *Text::setFontSlant(FontSlant slant) {
+        this->fontSlant = slant;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    FontSlant Text::getFontSlant() {
+        return this->fontSlant;
+    }
+
+    Text *Text::setFontFamily(std::string fontFamily) {
+        this->fontFamily = fontFamily;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    std::string Text::getFontFamily() {
+        return this->fontFamily;
+    }
+
+    Text *Text::setFontEdging(FontEdging edging) {
+        this->fontEdging = edging;
+        this->font = std::nullopt;
+        return this;
+    }
+
+    FontEdging Text::getFontEdging() {
+        return this->fontEdging;
+    }
+
+    SkFontStyle::Slant Text::getSkFontStyleSlant() {
+        switch (this->fontSlant) {
+            case FontSlant::Italic:
+                return SkFontStyle::kItalic_Slant;
+            case FontSlant::Oblique:
+                return SkFontStyle::kOblique_Slant;
+            default:
+                return SkFontStyle::kUpright_Slant;
+        }
+    }
+
+    SkFont::Edging Text::getSkFontEdging() {
+        switch (this->fontEdging) {
+            case FontEdging::Alias:
+                return SkFont::Edging::kAlias;
+            case FontEdging::AntiAlias:
+                return SkFont::Edging::kAntiAlias;
+            default:
+                return SkFont::Edging::kSubpixelAntiAlias;
+        }
+    }
+
+    SkFontStyle Text::makeSkFontStyle() {
+        return SkFontStyle(this->fontWeight, this->fontWidth, this->getSkFontStyleSlant());
+    }
+
+    sk_sp<SkTypeface> Text::makeSkTypeface() {
+        return SkTypeface::MakeFromName(this->fontFamily.c_str(), this->makeSkFontStyle());
+    }
+
+    SkFont Text::makeSkFont() {
+        SkFont font;
+        font.setTypeface(this->makeSkTypeface());
+        font.setSize(this->fontSize * this->ctx->monitorPixelScale);
+        font.setScaleX(this->fontScale);
+        font.setSkewX(this->fontSkew);
+        font.setEdging(this->getSkFontEdging());
+        return font;
+    }
+
+    void Text::updateSkFont() {
+        if (!this->font) {
+            this->font = this->makeSkFont();
+        }
+    }
+
+    SkPaint Text::makeSkPaint() {
+        SkPaint paint;
+        paint.setColor(this->getFontColor());
+        return paint;
+    }
+
+    void Text::updateSkPaint() {
+        if (!this->paint) {
+            this->paint = this->makeSkPaint();
+        }
+    }
+
+    Size Text::getSize(ApplicationContext *ctx, Boundaries boundaries) {
+        this->ctx = ctx;
+        this->updateSkFont();
+        this->updateSkPaint();
+
+        SkRect textBounds;
+        this->font.value().measureText(this->text.c_str(), this->text.size(), SkTextEncoding::kUTF8, &textBounds, &this->paint.value());
+
+        float width = std::min(std::max(textBounds.width(), boundaries.min.width), boundaries.max.width);
+        float height = std::min(std::max(textBounds.height(), boundaries.min.height), boundaries.max.height);
+        return fitSizeInBoundaries({width, height}, boundaries);
+    }
+
+    void Text::paintBackground(ApplicationContext *ctx, SkCanvas *canvas, ElementRect rect) {
+        this->ctx = ctx;
+        this->updateSkFont();
+        this->updateSkPaint();
+
+        canvas->translate(0, rect.size.height);
+        canvas->drawString(this->text.c_str(), 0, 0, this->font.value(), this->paint.value());
+    }
+}
---
+++ src/library/elements/Text.h
@@ -0,0 +1,115 @@
+//
+// Created by noartem on 11.06.2022.
+//
+
+#ifndef ELEMENTOR_TEXT_H
+#define ELEMENTOR_TEXT_H
+
+#include "../Element.h"
+
+#include <include/core/SkFont.h>
+#include <include/core/SkFontStyle.h>
+
+#include <optional>
+
+namespace elementor::elements {
+    enum class FontSlant {
+        Upright,
+        Italic,
+        Oblique,
+    };
+
+    enum class FontEdging {
+        Alias,
+        AntiAlias,
+        SubpixelAntiAlias,
+    };
+
+    class Text : public Element {
+    public:
+        Text *setText(std::string text);
+
+        std::string setText();
+
+        Text *setFontColor(SkColor color);
+
+        Text *setFontColor(uint8_t r, uint8_t g, uint8_t b);
+
+        Text *setFontColor(std::string color);
+
+        SkColor getFontColor();
+
+        Text *setFontSize(float size);
+
+        float getFontSize();
+
+        Text *setFontSkew(float skew);
+
+        float getFontSkew();
+
+        Text *setFontScale(float scale);
+
+        float getFontScale();
+
+        Text *setFontWeight(float weight);
+
+        float getFontWeight();
+
+        Text *setFontWidth(float width);
+
+        float getFontWidth();
+
+        Text *setFontSlant(FontSlant slant);
+
+        FontSlant getFontSlant();
+
+        Text *setFontFamily(std::string fontFamily);
+
+        std::string getFontFamily();
+
+        Text *setFontEdging(FontEdging edging);
+
+        FontEdging getFontEdging();
+
+        Size getSize(ApplicationContext *ctx, Boundaries boundaries) override;    
+
+        void paintBackground(ApplicationContext *ctx, SkCanvas *canvas, ElementRect rect) override;
+
+    private:
+        ApplicationContext *ctx;
+        std::string text;
+        SkColor fontColor = SK_ColorBLACK;
+        float fontSize = 16.0;
+        float fontSkew = 0.0;
+        float fontScale = 1.0;
+        float fontWeight = SkFontStyle::kNormal_Weight;
+        float fontWidth = SkFontStyle::kNormal_Width;
+        FontSlant fontSlant = FontSlant::Upright;
+        std::string fontFamily;
+        FontEdging fontEdging = FontEdging::AntiAlias;
+
+        std::optional<SkFont> font;
+        std::optional<SkPaint> paint;
+
+        SkFontStyle::Slant getSkFontStyleSlant();
+
+        SkFont::Edging getSkFontEdging();
+
+        SkFontStyle makeSkFontStyle();
+
+        sk_sp<SkTypeface> makeSkTypeface();
+
+        SkFont makeSkFont();
+
+        SkPaint makeSkPaint();
+
+        void updateSkFont();
+
+        void updateSkPaint();
+    };
+
+    Text *text();
+}
+
+
+#endif //ELEMENTOR_TEXT_H
