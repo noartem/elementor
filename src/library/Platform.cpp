a5df2f71c77c7f086c4c97f6c1951d05a8d678b5
Noskov Artem <nowasmawesome@gmail.com>
2022-06-28 18:48:25 +0500

    Remove close app on escape

--- src/library/Platform.cpp
+++ src/library/Platform.cpp
@@ -150,10 +150,6 @@ namespace elementor {
         }
 
         glfwSetKeyCallback(window, [] (GLFWwindow *window, int key, int scancode, int action, int mods) {
-            if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
-                glfwSetWindowShouldClose(window, GLFW_TRUE);
-            }
-
             Platform *platform = getWindowPlatform(window);
             platform->onKeyboard(key, scancode, action, mods);
         });
