567d308c5fb418e55217c06d4c103689837a0ebf
Noskov Artem <nowasmawesome@gmail.com>
2023-01-29 13:31:35 +0500

    WIP: Refactor napi build

---
+++ src/napi/preinstall.mjs
@@ -0,0 +1,20 @@
+import * as util from "util";
+import { readFile, writeFile } from "fs/promises";
+import { exec as lameExec } from "child_process";
+const exec = util.promisify(lameExec);
+
+const BUILD_DIR_PATH_PREFIX = "../../../";
+
+const { stdout: libElementorInfo } = await exec("xmake show -t elementor");
+const libElementorPath = libElementorInfo
+  .split("\n")
+  .find((e) => e.includes("targetfile"))
+  .split(": ")[1]
+  .trim()
+  .split("\x1B")[0];
+
+const bindingFileValue = (await readFile("binding.gyp.template"))
+  .toString()
+  .replaceAll("$libElementor", `${BUILD_DIR_PATH_PREFIX}${libElementorPath}`);
+
+await writeFile("binding.gyp", bindingFileValue, { flag: "w" });
