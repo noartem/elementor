38a3814cce559ac0b72905f08b46c1eb4ef8f86f
Noskov Artem <nowasmawesome@gmail.com>
2023-02-03 20:23:53 +0500

    Edit n-api preinstall.mjs

--- src/napi/preinstall.mjs
+++ src/napi/preinstall.mjs
@@ -1,20 +1,21 @@
 import * as util from "util";
-import { readFile, writeFile } from "fs/promises";
-import { exec as lameExec } from "child_process";
+import {readFile, writeFile} from "fs/promises";
+import {exec as lameExec} from "child_process";
+
 const exec = util.promisify(lameExec);
 
 const BUILD_DIR_PATH_PREFIX = "../../../";
 
-const { stdout: libElementorInfo } = await exec("xmake show -t elementor");
+const {stdout: libElementorInfo} = await exec("xmake show -t elementor");
 const libElementorPath = libElementorInfo
-  .split("\n")
-  .find((e) => e.includes("targetfile"))
-  .split(": ")[1]
-  .trim()
-  .split("\x1B")[0];
+    .split("\n")
+    .find((e) => e.includes("targetfile"))
+    .split(": ")[1]
+    .trim()
+    .split("\x1B")[0];
 
-const bindingFileValue = (await readFile("binding.gyp.template"))
-  .toString()
-  .replaceAll("$libElementor", `${BUILD_DIR_PATH_PREFIX}${libElementorPath}`);
+const bindingFileValue = (await readFile("binding.template.gyp"))
+    .toString()
+    .replaceAll("$libElementor", `${BUILD_DIR_PATH_PREFIX}${libElementorPath}`);
 
-await writeFile("binding.gyp", bindingFileValue, { flag: "w" });
+await writeFile("binding.gyp", bindingFileValue, {flag: "w"});
