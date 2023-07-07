21eefee53a7ba0ca86d495d1a7d8b647d0a20071
Noskov Artem <nowasmawesome@gmail.com>
2023-07-07 22:11:48 +0500

    Fix ExampleAnimation.cpp

--- src/examples/gallery/ExampleAnimation.cpp
+++ src/examples/gallery/ExampleAnimation.cpp
@@ -74,40 +74,40 @@ AnimatedPositionExample::AnimatedPositionExample(const std::string& label, int d
 
     this->buttonPadding = padding();
 
-    this->element = clickable()
-        ->setChild(this->buttonPadding
-            ->setChild(hoverable()
-                ->setChild(alignWidth()
+    this->element = this->buttonPadding
+        ->setChild(alignWidth()
+            ->setChild(clickable()
+                ->setChild(hoverable()
                     ->setChild(width()
-                    ->setWidth(80)
-                    ->setChild(rounded()
-                        ->setRadius(8)
-                        ->setChild(this->buttonBackground
-                            ->setColor("#DEEDE6")
-                            ->setChild(padding()
-                                ->setPaddings(12, 18)
-                                ->setChild(height()
-                                    ->setHeight(12)
-                                    ->setChild(alignWidth()
-                                        ->setCoef(0.5, 0.5)
-                                        ->setChild(this->buttonText))))))))
-                ->onEnter([this] () { this->buttonBackground->setColor("#C2DDD3"); })
-                ->onLeave([this] () { this->buttonBackground->setColor("#DEEDE6"); })))
-        ->onClick([this] () {
-            if (this->buttonAnimation != nullptr) {
-                this->buttonAnimation->pause();
-                this->buttonAnimation = nullptr;
-                return;
-            }
+                        ->setWidth(80)
+                        ->setChild(rounded()
+                            ->setRadius(8)
+                            ->setChild(this->buttonBackground
+                                ->setColor("#DEEDE6")
+                                ->setChild(padding()
+                                    ->setPaddings(12, 18)
+                                    ->setChild(height()
+                                        ->setHeight(12)
+                                        ->setChild(alignWidth()
+                                            ->setCoef(0.5, 0.5)
+                                            ->setChild(this->buttonText)))))))
+                    ->onEnter([this] () { this->buttonBackground->setColor("#C2DDD3"); })
+                    ->onLeave([this] () { this->buttonBackground->setColor("#DEEDE6"); }))
+                ->onClick([this] () {
+                    if (this->buttonAnimation != nullptr) {
+                       this->buttonAnimation->pause();
+                       this->buttonAnimation = nullptr;
+                       return;
+                    }
 
-            this->buttonAnimation = animation(this->ctx, this->duration, [this] (float value) {
-                this->buttonText->setText(std::to_string((int) (value * 100)) + "%");
-                auto paddingLeft = std::max((this->rect.size.width / ctx->getPixelScale()) - 80, ZERO) * value;
-                this->buttonPadding->setPaddings(0, 0, 0, paddingLeft);
-            });
+                    this->buttonAnimation = animation(this->ctx, this->duration, [this] (float value) {
+                       this->buttonText->setText(std::to_string((int) (value * 100)) + "%");
+                       auto paddingLeft = std::max((this->rect.size.width / ctx->getPixelScale()) - 80, ZERO) * value;
+                       this->buttonPadding->setPaddings(0, 0, 0, paddingLeft);
+                    });
 
-            this->buttonAnimation->play();
-        });
+                    this->buttonAnimation->play();
+                })));
 }
 
 std::shared_ptr<Element> ExampleAnimation::getScene(std::shared_ptr<ApplicationContext> ctx) {
